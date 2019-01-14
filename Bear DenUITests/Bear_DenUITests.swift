bd27f7b9c503312c2d8a546d6937e1ff5b4fefdf
2019 Brandon Garrison <garrisbj@lcms-macbook-pro-5.local>
2019-01-14 07:37:19 -0600

    Initial Commit

---
+++ Bear DenUITests/Bear_DenUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Bear_DenUITests.swift
+//  Bear DenUITests
+//
+//  Created by 2019 Brandon Garrison on 1/14/19.
+//  Copyright © 2019 Gabaro 2019. All rights reserved.
+//
+
+import XCTest
+
+class Bear_DenUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
