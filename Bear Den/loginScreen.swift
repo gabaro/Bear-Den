18fecb5b40eff8e8db9ec47099d3d49f8e9863f5
2019 Brandon Garrison <garrisbj@lcms-macbook-pro-5.local>
2019-02-14 07:32:01 -0600

    update

---
+++ Bear Den/loginScreen.swift
@@ -0,0 +1,21 @@
+//
+//  loginScreen.swift
+//  Bear Den
+//
+//  Created by 2019 Brandon Garrison on 1/30/19.
+//  Copyright © 2019 Gabaro 2019. All rights reserved.
+//
+
+import UIKit
+
+class loginScreen: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    
+}
